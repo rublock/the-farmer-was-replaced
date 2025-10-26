1ec11b588de982053da689b521f02f8ce9fc21cc
maksim.evlanov <maksim.evlanov@red-soft.ru>
2025-10-26 16:19:24 +0300

    Sunflowers

---
+++ hats.py
@@ -0,0 +1,13 @@
+def change_my_hat(i):
+
+	if i == 0:
+		change_hat(Hats.Gray_Hat)
+	elif i == 1:
+		change_hat(Hats.Purple_Hat)
+	elif i == 2:
+		change_hat(Hats.Green_Hat)
+	else:
+		change_hat(Hats.Brown_Hat)
+		
+if __name__ == "__main___":
+	change_hat(i)
--- main.py
+++ main.py
@@ -1,25 +1,25 @@
+from hats import change_my_hat
+
+clear()
+do_a_flip()
+
 while True:
 	for i in range(get_world_size()):
 		for j in range(get_world_size()):
 
-			move(North)
-
 			if can_harvest():
 				harvest()
+				plant(Entities.Tree)
 				if i % 2 != 1:
 					till()
-					# use_item(Items.Water)
-					plant(Entities.Carrot)
-				else:
-					plant(Entities.Bush)
+					use_item(Items.Water)
+					plant(Entities.Sunflower)
+			elif get_entity_type() == Entities.Dead_Pumpkin:
+				till()
+				use_item(Items.Water)
+			move(North)
+			
+			change_my_hat(i)
 
 		move(East)
-
-		if i == 0:
-			change_hat(Hats.Gray_Hat)
-		elif i == 1:
-			change_hat(Hats.Purple_Hat)
-		elif i == 2:
-			change_hat(Hats.Green_Hat)
-		else:
-			change_hat(Hats.Brown_Hat)
+		
